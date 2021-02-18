5e73619f954665055c3a2e1d98f18ac25003b398
KenDaryn <62947094+KenDaryn@users.noreply.github.com>
2021-02-18 17:41:31 +0600

    HW_Lesson_2

---
+++ HW_L_2/HW_L_2.go
@@ -0,0 +1,38 @@
+//package main
+//func main() {
+//	var a int
+//	fmt.Scanf("%d", &a)
+//	c:=a%10
+//	b:=a/10
+//	v:=b%10
+//	d:=a/100
+//	println("Итого: ",c+v+d)
+//}
+
+//func main() {
+//	var a, b int
+//	fmt.Scanf("%d", &a)
+//	fmt.Scanf("%d", &b)
+//	c := math.Pow(float64(a), 2.0) + math.Pow(float64(b), 2.0)
+//	fmt.Println("Гипотенуза треугольника:", c)
+//}
+
+//y = kx + b (k-?,b-?)
+//Ввод
+//3 4
+//5 6
+//x1=3,y1=4
+//x2=5,y2=6
+
+//import "fmt"
+//func main() {
+//	// y = kx + b
+//	var x1, y1, x2, y2 int
+//	fmt.Scanf("%d", &x1)
+//	fmt.Scanf("%d", &y1)
+//	fmt.Scanf("%d", &x2)
+//	fmt.Scanf("%d", &y2)
+//	k := (y1 - y2) / (x1 - x2)
+//	b := y2 - k * x2
+//	fmt.Println(k,b)
+//}
--- Test/1_L.go
+++ Test/1_L.go
@@ -1,5 +1,13 @@
 package Test
 
+import "fmt"
+//%d - число
+//%s - строка
+//это input как в Python
 func main ()  {
-
+	var a, b int
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	c:=a+b
+	fmt.Println(c)
 }
