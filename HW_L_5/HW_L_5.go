727720405b4f78457eb3c2069ff256ee39027a1e
KenDaryn <62947094+KenDaryn@users.noreply.github.com>
2021-03-05 21:34:24 +0600

    HW_L_6

--- HW_L_5/HW_L_5.go
+++ HW_L_5/HW_L_5.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //1) найти количество четных и нечетных элементов в массиве - готово
 //func main() {
 //	var n int
@@ -95,27 +93,147 @@ import "fmt"
 //	fmt.Println("Минимальная сумма", mini)
 //	fmt.Println("Разница между max и mini: ", max-mini)
 //}
-	//5) найти в массиве четные и нечетные элементы и
-	//поместить их в разные массивы и вывести эти массивы
+//5) найти в массиве четные и нечетные элементы и
+//поместить их в разные массивы и вывести эти массивы
+//
+//func main(){
+//		var n int
+//		fmt.Scanf("%d", &n)
+//		arr := [100] int{}
+//		for i := 0; i < n; i++ {
+//			var a int
+//			fmt.Scanf("%d", &a)
+//			arr[i] = a
+//}
+//	chet := [10] int{}
+//	nechet:=[10] int{}
+//	for i := 0; i < n; i++ {
+//		if arr[i]%2==0{
+//			chet[i]=arr[i]
+//		} else{
+//			nechet[i]=arr[i]
+//		}
+//	}
+//	fmt.Println("Четные = ", chet)
+//	fmt.Println("Не четные", nechet)
+//}
 
-	func main(){
-			var n int
-			fmt.Scanf("%d", &n)
-			arr := [100] int{}
-			for i := 0; i < n; i++ {
-				var a int
-				fmt.Scanf("%d", &a)
-				arr[i] = a
-	}
-		chet := [10] int{}
-		nechet:=[10] int{}
-		for i := 0; i < n; i++ {
-			if arr[i]%2==0{
-				chet[i]=arr[i]
-			} else{
-				nechet[i]=arr[i]
-			}
-		}
-		fmt.Println("Четные = ", chet)
-		fmt.Println("Не четные", nechet)
-	}
+//1) Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
+
+//func main() {
+//	var a, b, c int
+//	fmt.Scanf("%d", &a)
+//	fmt.Scanf("%d", &b)
+//	fmt.Scanf("%d", &c)
+//
+//	if a < b && b < c || c < b && b < a {
+//		fmt.Println("b", b)
+//	} else if b < a && a < c || c < a && a < b {
+//		fmt.Println("a", a)
+//	} else if b < c && c < a || a < c && c < b {
+//		fmt.Println("c", c)
+//	} else {
+//		fmt.Println("a,b,c", a, b, c)
+//	}
+//}
+
+//2) Из двух случайных чисел, одно из которых четное, а другое нечетное, определить и вывести на экран нечетное число.
+
+//func main() {
+//	var a, b int
+//	fmt.Scanf("%d", &a)
+//	fmt.Scanf("%d", &b)
+//	if a%2 != 0 && b%2 != 0 {
+//		fmt.Println("Оба нечетные", a, b)
+//	} else if b%2 != 0 {
+//		fmt.Println(b)
+//	} else if a%2 != 0 {
+//		fmt.Println(a)
+//	} else {
+//		fmt.Println("Все четные", a, b)
+//	}
+//}
+
+//3) В массиве найти максимальный элемент с четным индексом.
+//Другая формулировка задачи: среди элементов массива с четными индексами, найти тот, который имеет максимальное значение.
+
+//func main() {
+//	var n int
+//	fmt.Scanf("%d", &n)
+//	arr := [100]int{}
+//	for i := 0; i < n; i++ {
+//		var a int
+//		fmt.Scanf("%d", &a)
+//		arr[i] = a
+//	}
+//	max := 0
+//	for i := 0; i < n; i++ {
+//		if arr[i]%2 == 0 {
+//			if arr[i] > max {
+//				max = arr[i]
+//			}
+//		}
+//	}
+//	fmt.Println("Максимальная четная сумма: ", max)
+//}
+
+//4) Вывести элементы заданного массива в обратном порядке, то есть произвести реверс массива.
+
+//func main() {
+//	var n int
+//	fmt.Scanf("%d", &n)
+//	arr := [5]int{}
+//	for i := 0; i < n; i++ {
+//		var a int
+//		fmt.Scanf("%d", &a)
+//		arr[i] = a
+//	}
+//
+//	for i := n; i <= n; i-- {
+//		if i==-1{
+//			break
+//		}
+//		fmt.Println(arr[i])
+//
+//	}
+//}
+
+
+//5) Все элементы массива поделить на значение наибольшего элемента этого массива.
+//И сохранить в текущий массив
+
+//func main() {
+//	var n int
+//	fmt.Scanf("%d", &n)
+//	arr := [5]int{}
+//	for i := 0; i < n; i++ {
+//		var a int
+//		fmt.Scanf("%d", &a)
+//		arr[i] = a
+//	}
+//	max := 0
+//	for i := 0; i < n; i++ {
+//		if arr[i] > max {
+//			max = arr[i]
+//		}
+//	}
+//	for i := 0; i < n; i++ {
+//		arr[i] = arr[i] / max
+//	}
+//	fmt.Println(arr)
+//}
+
+//1.Напишите формулу чтобы вычислять последний индекс массива, формула должна работать для всех массивов
+//
+//func main() {
+//	var n int
+//	fmt.Scanf("%d", &n)
+//	arr := [5]int{}
+//	for i := 0; i < n; i++ {
+//		var a int
+//		fmt.Scanf("%d", &a)
+//		arr[i] = a
+//	}
+//	fmt.Println(len(arr)-1)
+//}
+//
