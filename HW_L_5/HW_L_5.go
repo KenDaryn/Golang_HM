58310d5d1a2d8b9df512debf32c4ea006bbdde22
KenDaryn <62947094+KenDaryn@users.noreply.github.com>
2021-03-17 18:20:20 +0600

    HW_L_9

--- HW_L_5/HW_L_5.go
+++ HW_L_5/HW_L_5.go
@@ -237,3 +237,36 @@ package main
 //	fmt.Println(len(arr)-1)
 //}
 //
+
+
+//package main
+
+import "fmt"
+
+func GetB(c []int) int {
+	a := 0
+	b := 0
+	for i := 0; i < len(c); i++ {
+		if c[i] < 1 {
+			a = 0
+		} else {
+			a = a + 1
+		}
+		if a > b {
+			b = a
+		}
+	}
+	return b
+}
+func printB(d []int) {
+	number := GetB(d)
+	fmt.Println("Длина самой продолжительной оттепели: ", number)
+}
+func main() {
+	arr1 := []int{-20, 30, -40, 50, 10, -10,12,12,12}
+	arr2 := []int{10, 20, 30, 1, -10, 1, 2, 3}
+	arr3 := []int{-10, 0, -10, 0, -10}
+	printB(arr1)
+	printB(arr2)
+	printB(arr3)
+}
